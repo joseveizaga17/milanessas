9c792437e8801d82797d0afb007c9d3a0840e480
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-06-27 03:29:30 +0000

    bootstrap agregado

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=["GET"])
-def entry():
+def home():
     return """
     <h1>Welcome to how to create milanga</h1>
     """
-@app.route('/creates', methods=['GET'])
+@app.route('/create', methods=['GET'])
 def create():
     return """
     <h2>como crear tu propia milanga paso por paso</h2>
