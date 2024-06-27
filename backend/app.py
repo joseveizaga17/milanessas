3ad9b65667f93b04fbe92e9a8534c24bc745cc70
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-06-27 02:50:52 +0000

    algunos endpoints agregados

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-import flask from Flask
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -8,4 +8,11 @@ def entry():
     return """
     <h1>Welcome to how to create milanga</h1>
     """
-           
+@app.route('/creates', methods=['GET'])
+def create():
+    return """
+    <h2>como crear tu propia milanga paso por paso</h2>
+    """
+@app.route('/invents', methods=['GET'])
+def invents():
+    return """<h1>invents milanga</h1>"""
