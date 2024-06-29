9204f89c753f12b20488d8889e96c49e7a4938c6
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-06-29 23:47:15 +0000

    columnas agregadas, correccion letras

--- backend/bd/app.py
+++ backend/bd/app.py
@@ -3,11 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Milanga(db.model):
+class Milanga(db.Model):
     __tablename__ = 'milanga'
+    id = db.Column(db.Integer, primary_key=True)
+    tipo_pan = db.Column(db.String(500), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
 
 
-
-class Usuario(db.model):
+class Usuario(db.Model):
     __tablename__ = 'usuarios'
-    
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(500), nullable=False)
