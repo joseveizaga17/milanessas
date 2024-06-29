aec832d7226d693e1ab8c21fc38436f48c19d12d
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-06-29 23:34:00 +0000

    correcion URI,db init

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-#from app import db, Milanga, Usuario
+from bd.app import db, Milanga, Usuario
 
 app = Flask(__name__)
 CORS(app)
 
-#app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2-binary://postgres:manu17@localhost/tp2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.route('/', methods=["GET"])
 def home():
-    return """
-    <h1>Welcome to how to create milanga</h1>
-    """
+    #try:
+    #    return  
+    #except:
+    #    return 
+    return """<h1>Welcome to how to create milanga</h1>"""
+
 @app.route('/create', methods=['GET'])
 def create():
     return """
@@ -21,3 +24,11 @@ def create():
 @app.route('/invents', methods=['GET'])
 def invents():
     return """<h1>invents milanga</h1>"""
+
+if __name__ == '__main__':
+    print('iniciando...')
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', debug=True, port=5000)
+    print('iniciado!!!')
