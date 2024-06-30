2c0c81885adfdf32e845a7840c3e1308172c4f8f
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-06-30 19:06:50 +0000

    error post

--- backend/app.py
+++ backend/app.py
@@ -15,11 +15,26 @@ def home():
     #    return 
     return """<h1>Welcome to how to create milanga</h1>"""
 
-@app.route('/create', methods=['GET'])
-def create():
-    return """
-    <h2>como crear tu propia milanga paso por paso</h2>
-    """
+@app.route('/create', methods=['POST'])
+def create_sandwich():
+    try:
+        content = request.json
+        pan = content.get('tipo_pan')
+        carne = content.get('tipo_carne')
+        ensalada = content.get('tipo_ensalada')
+        milanesa = content.get('tipo_milanesa')
+        papas = content.get('tipo_papas')
+
+        nuevo_sandwich = Milanga(tipo_pan=pan, tipo_carne=carne, tipo_ensalada=ensalada, tipo_milanesa=milanesa, tipo_papas=papas)
+
+        db.session.add(nuevo_sandwich)
+        db.session.commit()
+        return jsonify({'milanesas':{'id': nuevo_sandwich.id, 'pan': nuevo_sandwich.pan,'carne' : nuevo_sandwich.carne}, 'ensalada' : nuevo_sandwich.ensalada, 'milanesa' : nuevo_sandwich.milanesa, 'papas' : nuevo_sandwich.papas}); print(type(db))
+    except:
+        return
+    #jsonify({'alert' : 'no se pudo crear un nuevo sandwich'})
+    
+    #return """<h2>como crear tu propia milanga paso por paso</h2>"""
 @app.route('/invents', methods=['GET'])
 def invents():
     return """<h1>invents milanga</h1>"""
