8d8d2c304898a52945a4f13eb9cad3020b8542f7
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-06-30 19:06:15 +0000

    posibles columnas, agregadas

--- backend/bd/app.py
+++ backend/bd/app.py
@@ -7,10 +7,16 @@ class Milanga(db.Model):
     __tablename__ = 'milanga'
     id = db.Column(db.Integer, primary_key=True)
     tipo_pan = db.Column(db.String(500), nullable=False)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    tipo_carne = db.Column(db.String(500), nullable=False)
+    tipo_ensalda = db.Column(db.String(500))
+    tipo_milanesa = db.Column(db.String(500), nullable=False)
+    tipo_papas = db.Column(db.String(500))
+    
+    #usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
 
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(500), nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
