cf9049121335b9f5e5cc3da0718daf01c4dcb2cc
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-07-03 22:27:27 +0000

    se agregó mas columnas

--- backend/bd/app.py
+++ backend/bd/app.py
@@ -6,17 +6,20 @@ db = SQLAlchemy()
 class Milanga(db.Model):
     __tablename__ = 'milanga'
     id = db.Column(db.Integer, primary_key=True)
-    tipo_pan = db.Column(db.String(500), nullable=False)
-    tipo_carne = db.Column(db.String(500), nullable=False)
-    tipo_ensalada = db.Column(db.String(500))
-    tipo_milanesa = db.Column(db.String(500), nullable=False)
-    tipo_papas = db.Column(db.String(500))
-
-    #usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
-
+    pan = db.Column(db.String(500), nullable=False)
+    carne = db.Column(db.String(500), nullable=False)
+    ensalada = db.Column(db.String(500))
+    milanesa = db.Column(db.String(500), nullable=False)
+    papas = db.Column(db.String(500))
+    #fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
+    #usuario_id = db.Column(db.Integer, db.ForeingKey('usuario.id'))
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(500), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
+    imagen = db.Column(db.String(500), nullable=False)
+    #email = db.Column(db.String(500), unique=True, nullable=False)
+    #fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
+    #milangas = db.relationship("Milanga")
