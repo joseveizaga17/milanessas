a133683030df7f182bae36acfe66d8b90ce72b41
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-07-04 13:04:52 +0000

    datetime agregado

--- backend/bd/app.py
+++ backend/bd/app.py
@@ -1,25 +1,26 @@
-#import datetime
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Milanga(db.Model):
-    __tablename__ = 'milanga'
+    __tablename__ = 'milangas'
     id = db.Column(db.Integer, primary_key=True)
     pan = db.Column(db.String(500), nullable=False)
-    carne = db.Column(db.String(500), nullable=False)
-    ensalada = db.Column(db.String(500))
     milanesa = db.Column(db.String(500), nullable=False)
+    ensalada = db.Column(db.String(500))
+    coccion = db.Column(db.String(500), nullable=False)
     papas = db.Column(db.String(500))
-    #fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
-    #usuario_id = db.Column(db.Integer, db.ForeingKey('usuario.id'))
+    fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(500), nullable=False)
+    apellido = db.Column(db.String(500), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
     imagen = db.Column(db.String(500), nullable=False)
-    #email = db.Column(db.String(500), unique=True, nullable=False)
-    #fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
-    #milangas = db.relationship("Milanga")
+    email = db.Column(db.String(500), unique=True, nullable=False)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
+    milangas = db.relationship("Milanga")
