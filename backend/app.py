b535f8c3bd159f456f3b09b8ffad502d778a8f51
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-07-04 13:02:23 -0300

    puerto 5500

--- backend/app.py
+++ backend/app.py
@@ -195,4 +195,4 @@ if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run(host='127.0.0.1', debug=True, port=5000)
+    app.run(host='127.0.0.1', debug=True, port=5500)
