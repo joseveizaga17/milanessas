2683a71ed74413552870a5b1ea9d7d32f9d57ab8
Power 345 <facundo.t345@gmail.com>
2024-07-12 20:06:31 -0300

    poniendo el codigo js al archivo invent

--- frontend/invent/invent.js
+++ frontend/invent/invent.js
@@ -1,2 +1,83 @@
+function data_receive(data){
 
+    let content = document.getElementById("list");
+
+    //Aqui es donde creo cada elemento de cada tabla
+
+    for(let index = 0; index < data.length; index++){
+      let lista = document.createElement("li");
+      lista.setAttribute("class", "list-group-item list-group-item-dark");
+
+      let header = document.createElement("div");
+      header.setAttribute("class","d-flex w-100 justify-content-between");
+
+      let nombre = document.createElement("h5");
+      nombre.setAttribute("class", "mb-1");
+      nombre.append(data[index].milanesa);
+
+      let fecha = document.createElement("small");
+      fecha.setAttribute("class", "text-body-secondary");
+      fecha.append(data[index].fecha_creacion);
+
+      let ingredientes = document.createElement("p");
+      ingredientes.setAttribute("class", "mb-1");
+      ingredientes.append(`Contiene ${data[index].pan} con ${data[index].milanesa} ${data[index].coccion}, con ${data[index].ensalada} acompañado con ${data[index].papas}`);
+
+      let footer = document.createElement("div");
+      footer.setAttribute("class", "d-flex w-100 justify-content-between");
+
+      let usuario = document.createElement("small");
+      usuario.setAttribute("class", "align-self-center");
+      usuario.append(`Usuario ${data[index].usuario_id}`);
+
+      let boton = document.createElement("button");
+      boton.setAttribute("class", "btn btn-danger");
+      boton.setAttribute("type", "submit");
+      boton.onclick = function(){eliminar_sandwich(data[index])};
+      boton.innerText = "Eliminar";
+
+
+      header.append(nombre);
+      header.append(fecha);
+
+      lista.append(header);
+      lista.append(ingredientes);
+      
+      footer.append(usuario);
+      footer.append(boton);
+
+      lista.append(footer);
+      content.append(lista);
+    }
+   
+  }
+
+  function delete_response(data){
+    window.location.href = window.location.href;
+  }
+
+  function handle_error(error){
+    console.log("Error", error);
+  }
+
+
+  function eliminar_sandwich(sand){
+
+    const confirmacion = confirm(`Estas seguro de eliminar el sandwich ${sand.id}?`);
+
+    if(!confirmacion){
+      return
+    }
+
+    fetch(`http://127.0.0.1:5500/delete/${sand.id}`, 
+    { method: "DELETE" })
+      .then((res) => res.json())
+      .then(delete_response)
+      .catch(handle_error);
+  }
+
+  fetch('http://127.0.0.1:5500/sandwichs')
+    .then(response => response.json())
+    .then(data_receive)
+    .catch(handle_error);
 
