f08908549f94166a8a43297c3ce1c49cd16942ac
Power 345 <facundo.t345@gmail.com>
2024-07-13 18:41:35 -0300

    creando un js para poder cambiar los tipos de ingredientes

---
+++ frontend/create/opciones.js
@@ -0,0 +1,139 @@
+function boton1(){
+    let formulario = document.getElementById("form-sandwich");
+
+    let pan1 = document.getElementById("pan1").onclick = "";
+    let pan2 = document.getElementById("pan2").onclick = "";
+    let pan3 = document.getElementById("pan3").onclick = "";
+
+    let boton = document.createElement("button");
+    boton.setAttribute("type", "button");
+    boton.setAttribute("class", "btn btn-primary ms-4 mb-3");
+    boton.setAttribute("id", "boton1");
+    boton.innerText = "Siguiente";
+    boton.onclick = function(){siguiente1()};
+
+    formulario.append(boton);
+}
+
+function siguiente1(){
+    let boton1 = document.getElementById("boton1")
+    boton1.setAttribute("class", "d-none");
+
+    let pan = document.getElementById("pan");
+    pan.setAttribute("class", "d-none");
+
+    let carne = document.getElementById("carne");
+    carne.classList.remove("d-none");
+}
+
+function boton2(){
+    let formulario = document.getElementById("form-sandwich");
+
+    let pan1 = document.getElementById("pan1").onclick = "";
+    let pan2 = document.getElementById("pan2").onclick = "";
+    let pan3 = document.getElementById("pan3").onclick = "";
+
+    let boton = document.createElement("button");
+    boton.setAttribute("type", "button");
+    boton.setAttribute("class", "btn btn-primary ms-4 mb-3");
+    boton.setAttribute("id", "boton1");
+    boton.innerText = "Siguiente";
+    boton.onclick = function(){siguiente2()};
+
+    formulario.append(boton);
+}
+
+function siguiente2(){
+    let boton1 = document.getElementById("boton1")
+    boton1.setAttribute("class", "d-none");
+
+    let pan = document.getElementById("pan");
+    pan.setAttribute("class", "d-none");
+
+    let carne = document.getElementById("carne");
+    carne.classList.remove("d-none");
+}
+
+function boton3(){
+    let formulario = document.getElementById("form-sandwich");
+
+    let pan1 = document.getElementById("pan1").onclick = "";
+    let pan2 = document.getElementById("pan2").onclick = "";
+    let pan3 = document.getElementById("pan3").onclick = "";
+
+    let boton = document.createElement("button");
+    boton.setAttribute("type", "button");
+    boton.setAttribute("class", "btn btn-primary ms-4 mb-3");
+    boton.setAttribute("id", "boton1");
+    boton.innerText = "Siguiente";
+    boton.onclick = function(){siguiente3()};
+
+    formulario.append(boton);
+}
+
+function siguiente3(){
+    let boton1 = document.getElementById("boton1")
+    boton1.setAttribute("class", "d-none");
+
+    let pan = document.getElementById("pan");
+    pan.setAttribute("class", "d-none");
+
+    let carne = document.getElementById("carne");
+    carne.classList.remove("d-none");
+}
+
+function boton4(){
+    let formulario = document.getElementById("form-sandwich");
+
+    let pan1 = document.getElementById("pan1").onclick = "";
+    let pan2 = document.getElementById("pan2").onclick = "";
+    let pan3 = document.getElementById("pan3").onclick = "";
+
+    let boton = document.createElement("button");
+    boton.setAttribute("type", "button");
+    boton.setAttribute("class", "btn btn-primary ms-4 mb-3");
+    boton.setAttribute("id", "boton1");
+    boton.innerText = "Siguiente";
+    boton.onclick = function(){siguiente4()};
+
+    formulario.append(boton);
+}
+
+function siguiente4(){
+    let boton1 = document.getElementById("boton1")
+    boton1.setAttribute("class", "d-none");
+
+    let pan = document.getElementById("pan");
+    pan.setAttribute("class", "d-none");
+
+    let carne = document.getElementById("carne");
+    carne.classList.remove("d-none");
+}
+
+function boton5(){
+    let formulario = document.getElementById("form-sandwich");
+
+    let pan1 = document.getElementById("pan1").onclick = "";
+    let pan2 = document.getElementById("pan2").onclick = "";
+    let pan3 = document.getElementById("pan3").onclick = "";
+
+    let boton = document.createElement("button");
+    boton.setAttribute("type", "button");
+    boton.setAttribute("class", "btn btn-primary ms-4 mb-3");
+    boton.setAttribute("id", "boton1");
+    boton.innerText = "Siguiente";
+    boton.onclick = function(){siguiente5()};
+
+    formulario.append(boton);
+}
+
+function siguiente5(){
+    let boton1 = document.getElementById("boton1")
+    boton1.setAttribute("class", "d-none");
+
+    let pan = document.getElementById("pan");
+    pan.setAttribute("class", "d-none");
+
+    let carne = document.getElementById("carne");
+    carne.classList.remove("d-none");
+}
