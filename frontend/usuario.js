118c6f02c1a9ce1bfe3925723241c02b36abaefe
Power 345 <facundo.t345@gmail.com>
2024-07-21 16:17:38 -0300

    solucionando un error en mostrar usuario y añadiendo la opcion de salir usuario

--- frontend/usuario.js
+++ frontend/usuario.js
@@ -7,12 +7,40 @@ function data_received(data){
     imagen.setAttribute("witdh", 30);
     imagen.setAttribute("height", 50);
 
-    let name = document.createElement("p");
+    /*let name = document.createElement("p");
     name.setAttribute("class", "fw-bold text-capitalize ms-1 align-self-center")
-    name.innerText = `${data.nombre} ${data.apellido}`;
+    name.innerText = `${data.nombre} ${data.apellido}`;*/
 
+    let drop = document.createElement("div");
+    drop.setAttribute("class", "btn-group me-2");
+
+    let nombre = document.createElement("button");
+    nombre.setAttribute("class", "btn btn-dark dropdown-toggle");
+    nombre.setAttribute("data-bs-toggle", "dropdown");
+    nombre.setAttribute("aria-expanded", "false");
+    nombre.innerText = `${data.nombre} ${data.apellido}`;
+
+    let dropd = document.createElement("ul");
+    dropd.setAttribute("class", "dropdown-menu");
+
+    let lista1 = document.createElement("li");
+    let a1 = document.createElement("a");
+    a1.setAttribute("class", "dropdown-item");
+    a1.setAttribute("href", "/frontend/index.html");
+    a1.innerText = "Salir";
+
+    drop.append(nombre)
+    lista1.append(a1);
+    dropd.append(lista1);
+    drop.append(dropd);
+    usuario.append(drop);
     usuario.append(imagen);
-    usuario.append(name);
+
+
+    
+    
+    
+
   }
 
 let id = new URLSearchParams(window.location.search).get("id");
@@ -29,3 +57,12 @@ let id = new URLSearchParams(window.location.search).get("id");
     .then(res => res.json())
     .then(data_received)
     .catch(error => console.log("errores: ", error));
+    /*    <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+*/
